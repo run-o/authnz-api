712ab26331f13933e01522f38974460df451f4ed
Renaud Amar <renaud.amar@gmail.com>
2025-01-13 15:32:34 +0100

    Handle RLS exception when creating user data

--- app/api/routes/user_data.py
+++ app/api/routes/user_data.py
@@ -1,8 +1,10 @@
 import logging
 from typing import Any
+import psycopg2
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import ProgrammingError
 
 from app.db import get_db
 from app.api.deps import AuthenticationRequired
@@ -36,9 +38,15 @@ def get_user_data(email: str, db: Session = Depends(get_db)) -> Any:
 def create_user_data(data_create: schemas.UserDataCreate, db: Session = Depends(get_db)) -> Any:
     """ Create new data for user. """
     try:
-        new_user = crud.create_user_data(db, data_create)
+        new_data = crud.create_user_data(db, data_create)
+    except ProgrammingError as exc:
+        if "InsufficientPrivilege" in str(exc):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                                detail=f"Cannot create data for user {data_create.user_id}.")
+        else:
+            raise
     except Exception as exc:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(exc))
 
-    return new_user
+    return new_data
 
