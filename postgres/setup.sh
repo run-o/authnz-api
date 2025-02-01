fb298a0cc63a296512f327e1c8fe0ffa37821d03
Renaud Amar <renaud.amar@gmail.com>
2025-02-01 18:52:48 +0100

    Fix postgres roles setup
    Correctly set up the default privileges on future objects for the API user:
    - only grant SELECT, INSERT, UPDATE, DELETE
    - don't allow DROP and TRUNCATE
    - grant SELECT, UPDATE, USAGE on sequences

--- postgres/setup.sh
+++ postgres/setup.sh
@@ -10,11 +10,21 @@ psql -U postgres authnz << EOF
 CREATE EXTENSION "uuid-ossp";
 
 CREATE USER authnz_user WITH PASSWORD 'authnz_user';
+
+-- grant permissions on all existing tables
 GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES in SCHEMA public TO authnz_user;
+
+-- grant permissions on sequences
 GRANT SELECT, UPDATE, USAGE ON ALL SEQUENCES in SCHEMA public TO authnz_user;
+
+-- grant funtion execution rights
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO authnz_user;
 
-ALTER DEFAULT PRIVILEGES FOR ROLE authnz_owner IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO authnz_user;
-ALTER DEFAULT PRIVILEGES FOR ROLE authnz_owner IN SCHEMA public GRANT USAGE ON SEQUENCES TO authnz_user;
-ALTER DEFAULT PRIVILEGES FOR ROLE authnz_owner IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO authnz_user;
+-- set default privileges for future tables, sequences and functions:
+ALTER DEFAULT PRIVILEGES FOR ROLE authnz_owner IN SCHEMA public
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO authnz_user;
+ALTER DEFAULT PRIVILEGES FOR ROLE authnz_owner IN SCHEMA public
+    GRANT SELECT, UPDATE, USAGE ON SEQUENCES TO authnz_user;
+ALTER DEFAULT PRIVILEGES FOR ROLE authnz_owner IN SCHEMA public
+    GRANT EXECUTE ON FUNCTIONS TO authnz_user;
 EOF
